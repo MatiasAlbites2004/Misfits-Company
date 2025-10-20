c664cabc3fb72b1857ac13d93f9f1ff35859992f
MatiasAlbites2004 <matiasalbites1512@gmail.com>
2025-10-20 10:40:06 -0500

    Feat: Deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// 🧠 Asegúrate de reemplazar 'Misfits-Company' por el nombre exacto de tu repo en GitHub
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/Misfits-Company/",
+});
